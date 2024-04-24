c62a1b25e19e767296e45831e99fa00b75102389
Vladislav <vlad.b8239@gmail.com>
2024-04-24 18:22:20 +0300

    Added exception handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <QApplication>
+#include <QMessageBox>
 
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
+try
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.showMaximized();
     return a.exec();
+}
+catch (std::bad_alloc &exception)
+{
+    QMessageBox::critical(nullptr, "Ошибка выделения памяти", exception.what());
+}
+catch (...)
+{
+    QMessageBox::critical(nullptr, "Ошибка", "Произошла ошибка, пожалуйста перезапустите программу");
 }
