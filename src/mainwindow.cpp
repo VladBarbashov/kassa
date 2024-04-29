808e6d03df1600100a4d2359017249e8669f9fb0
Vladislav <vlad.b8239@gmail.com>
2024-04-29 19:34:51 +0300

    Added the ability to search for flights

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 
+#include <QLayoutItem>
 #include <QStringListModel>
 
 #include "dbsettings.h"
+#include "flight.h"
 #include "ui_mainwindow.h"
+
+void clearLayt(QLayout *layout);
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -39,7 +43,7 @@ void MainWindow::on_dbSettingsAct_triggered()
 
 void MainWindow::on_departureCityLEdit_textEdited(const QString &departureCity)
 {
-    if (dbManager && (dbManager->connectionState() == db::DBState::OK))
+    if (dbManager)
     {
         dbManager->performQuery(std::format("SELECT departureCity FROM flights WHERE departureCity LIKE '{}%' ORDER BY departureCity", departureCity.toStdString()).c_str());
         QStringList list;
@@ -53,7 +57,7 @@ void MainWindow::on_departureCityLEdit_textEdited(const QString &departureCity)
 
 void MainWindow::on_arrivalCityLEdit_textEdited(const QString &arrivalCity)
 {
-    if (dbManager && (dbManager->connectionState() == db::DBState::OK))
+    if (dbManager)
     {
         dbManager->performQuery(std::format("SELECT arrivalCity FROM flights WHERE arrivalCity LIKE '{}%' ORDER BY arrivalCity", arrivalCity.toStdString()).c_str());
         QStringList list;
@@ -79,5 +83,40 @@ void MainWindow::on_backFlightChk_stateChanged(int state)
 
 void MainWindow::on_searchPBtn_clicked()
 {
+    clearLayt(ui->flightsLayt);
+    if (dbManager)
+    {
+        dbManager->performQuery(std::format("SELECT f.flightNumber, f.departureCity, f.arrivalCity, f.departureTime, f.arrivalTime, f.planeType, fc.class, c.price "
+                                            "FROM flights AS f JOIN cost AS c ON f.flightNumber=c.flightNumber JOIN crew AS cr ON f.flightNumber=cr.flightNumber JOIN flightClasses AS fc ON c.classId=fc.classId "
+                                            "WHERE f.departureCity='{}' AND f.arrivalCity='{}' AND f.departureTime LIKE '{}%' AND fc.class='{}' "
+                                            "ORDER BY f.departureCity"
+                                            , ui->departureCityLEdit->text().toStdString(), ui->arrivalCityLEdit->text().toStdString()
+                                            , ui->departureDateDEdit->date().toString(Qt::DateFormat::ISODate).toStdString(), ui->flightClassCmb->currentText().toStdString()).c_str());
+        QList<QVariant> result = dbManager->nextRow();
+        while (!result.empty())
+        {
+            QList<QVariant> flightData(result.cbegin(), result.cbegin() + 8);
+            QList<QVariant> crewData(result.cbegin() + 8, result.cend());
+            ui->flightsLayt->addWidget(new Flight(flightData, crewData), 0, Qt::AlignTop);
+            result = dbManager->nextRow();
+        }
+    }
+}
 
+void clearLayt(QLayout *layout)
+{
+    if (layout != NULL)
+    {
+        QLayoutItem *item;
+        while((item = layout->takeAt(0)))
+        {
+            if (item->layout())
+            {
+                clearLayt(item->layout());
+                delete item->layout();
+            }
+            delete item->widget();
+            delete item;
+        }
+    }
 }
